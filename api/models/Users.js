a1b4819d4c685f073af39353a28ff477df32922b
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-01-01 10:07:54 +0700

    create users api, add auth, and jwtoken

---
+++ api/models/Users.js
@@ -0,0 +1,27 @@
+/**
+ * Users.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+     id: {
+        type: 'integer',
+        autoIncrement: true,
+        primaryKey: true
+     },
+     name: {
+        type: 'string'
+     },
+     email: {
+        type: 'string',
+        unique: true
+     },
+     password: {
+        type: 'string'
+     }
+  }
+};
